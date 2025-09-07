82aae1576529613cdaa2c1bf4610c88c6fe364d7
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2025-09-07 18:39:33 +0000

    refactor(utils): se agrega goto para evitar repeticion de logica en manejo de errores

--- utils/src/utils/cliente.c
+++ utils/src/utils/cliente.c
@@ -2,53 +2,51 @@
 
 int conectar_servidor(char *ip, char *puerto)
 {
-    struct addrinfo hints, *server_info;
+    struct addrinfo hints, *server_info = NULL;
+    int socket_cliente = -1;
+    int resultado_obtener_server_info;
+    int resultado_conexion;
 
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;       // IPv4
     hints.ai_socktype = SOCK_STREAM; // TCP
 
     // Arma la información de la dirección del socket
-    int resultado_obtener_server_info = getaddrinfo(
-        ip,
-        puerto,
-        &hints,
-        &server_info);
-
+    resultado_obtener_server_info = getaddrinfo(ip, puerto, &hints, &server_info);
     if (resultado_obtener_server_info != 0)
     {
-        return -1;
+        goto error;
     }
 
     // Crea el socket del cliente
-    int socket_cliente = socket(
-        server_info->ai_family,
-        server_info->ai_socktype,
-        server_info->ai_protocol);
-
+    socket_cliente = socket(server_info->ai_family,
+                            server_info->ai_socktype,
+                            server_info->ai_protocol);
     if (socket_cliente == -1)
     {
-        freeaddrinfo(server_info);
-        return -1;
+        goto borrar_addrinfo;
     }
 
     // Intenta conectarse al servidor
-    int resultado_conexion = connect(
-        socket_cliente,
-        server_info->ai_addr,
-        server_info->ai_addrlen);
-
+    resultado_conexion = connect(socket_cliente,
+                                 server_info->ai_addr,
+                                 server_info->ai_addrlen);
     if (resultado_conexion == -1)
     {
-        freeaddrinfo(server_info);
-        return -1;
+        goto borrar_socket;
     }
 
-    // Libera la memoria del puntero con la info. de dirección del servidor
     freeaddrinfo(server_info);
-
     return socket_cliente;
+
+borrar_socket:
+    close(socket_cliente);
+borrar_addrinfo:
+    freeaddrinfo(server_info);
+error:
+    return -1;
 }
+
 
 void liberar_conexion(int socket_cliente) {
     if (socket_cliente != -1) {
