5ee8617770a3bcb7ea22f3c468ef8dfd13f754ce
Daniel <dperalta@frba.utn.edu.ar>
2025-09-07 15:04:49 -0300

    feat - Inicialización servidor TCP. Crea el socket y lo bindea al puerto indicado

--- query_control/src/main.c
+++ query_control/src/main.c
@@ -1,6 +1,12 @@
 #include <utils/hello.h>
+#include <utils/server.h>
 
 int main(int argc, char* argv[]) {
     saludar("query_control");
+
+    // SOLO PARA TEST DE FUNCIÓN inicar_servidor hardcodeo ip y puerto
+    int socket;
+    socket = iniciar_servidor("127.0.0.1", "8001");
+    printf("Socket %d creado con exito!", socket);
     return 0;
 }
