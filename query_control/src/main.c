429e227936e53f9eeec67330d0b70d7de714b427
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2025-09-13 11:54:11 -0300

    Mejor parseo de prioridades

--- query_control/src/main.c
+++ query_control/src/main.c
@@ -15,15 +15,21 @@ int main(int argc, char* argv[])
 {
     char* config_filepath = argv[1];
     char* query_filepath = argv[2];
-    char* priority = argv[3];
-    // PATH_MAX para la ruta del query + 1 para el delimiter 0x1F + 9 para la prioridad (max 99999999) + '\0'
-    char send_buffer[PATH_MAX+1+9];
+    int priority = atoi(argv[3]);
+    // PATH_MAX para la ruta del query + 1 para el delimiter 0x1F + 11 para la prioridad (max int) + '\0'
+    char send_buffer[PATH_MAX+1+11];
     char response_buffer[256];
     int retval = 0;
 
     if (argc != 4) {
         printf("[ERROR]: Se esperaban ruta al archivo de configuracion, archivo de query y prioridad\nUso: %s [archivo_config] [archivo_query] [prioridad]\n", argv[0]);
         retval = -1;
+        goto error;
+    }
+
+    if (priority < 0) {
+        printf("[ERROR]: La prioridad no puede ser negativa\n");
+        retval = -8;
         goto error;
     }
 
@@ -66,9 +72,9 @@ int main(int argc, char* argv[])
     }
 
     log_info(logger, "## Conexión al Master exitosa. IP: %s, Puerto: %s", query_control_config->ip, query_control_config->port);
-    log_info(logger, "## Solicitud de ejecución de Query: %s, prioridad: %s", query_filepath, priority);
+    log_info(logger, "## Solicitud de ejecución de Query: %s, prioridad: %d", query_filepath, priority);
 
-    snprintf(send_buffer, sizeof(send_buffer), "%s\x1F%s", query_filepath, priority);
+    snprintf(send_buffer, sizeof(send_buffer), "%s\x1F%d", query_filepath, priority);
     if (send(master_socket, send_buffer, strlen(send_buffer), 0) == -1)
     {
         log_error(logger, "Error al enviar el path de query y prioridad al master");
