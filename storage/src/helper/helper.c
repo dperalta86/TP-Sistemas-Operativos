ecd93495189b973c4b4e904f4a20b3ec27f89a39
David Achas <dachassilva@frba.utn.edu.ar>
2025-09-17 15:38:26 +0000

    Agrega función para obtener el enum log lvel a partir del string del archivo config.

--- storage/src/helper/helper.c
+++ storage/src/helper/helper.c
@@ -10,3 +10,30 @@ size_t strlcpy(char *dest, const char *source, size_t size) {
 
     return strlen(source);
 }
+
+t_log_level log_level_from_string(char* level) {
+    if (level == NULL) {
+        fprintf(stderr, "Error: El string de nivel de log es nulo.\n");
+        return LOG_LEVEL_INFO; // Valor por defecto
+    }
+
+    if (strcmp(level, "TRACE") == 0) {
+        return LOG_LEVEL_TRACE;
+    }
+    if (strcmp(level, "DEBUG") == 0) {
+        return LOG_LEVEL_DEBUG;
+    }
+    if (strcmp(level, "INFO") == 0) {
+        return LOG_LEVEL_INFO;
+    }
+    if (strcmp(level, "WARNING") == 0) {
+        return LOG_LEVEL_WARNING;
+    }
+    if (strcmp(level, "ERROR") == 0) {
+        return LOG_LEVEL_ERROR;
+    }
+    
+    // Si el string no coincide con ningún nivel conocido
+    fprintf(stderr, "Advertencia: Nivel de log desconocido '%s'. Usando INFO por defecto.\n", level);
+    return LOG_LEVEL_INFO;
+}
--- storage/src/helper/helper.h
+++ storage/src/helper/helper.h
@@ -2,7 +2,9 @@
 #define STROAGE_HELPER_H_
 
 #include <string.h>
+#include <commons/log.h>
 
 size_t strlcpy(char *dest, const char *src, size_t size);
+t_log_level log_level_from_string(char* level);
 
 #endif
