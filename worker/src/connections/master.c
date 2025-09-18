0633f03b2e81e4fe692c8e4e938df6ce2ec41841
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2025-09-18 01:31:26 +0000

    refactor: se mejoran los logs del handshake

--- worker/src/connections/master.c
+++ worker/src/connections/master.c
@@ -16,23 +16,24 @@ int handshake_with_master(const char *master_ip, const char *master_port)
     buffer_write_uint8(buffer, OP_WORKER_HANDSHAKE_REQ);
     t_package *request = package_create(OP_WORKER_HANDSHAKE_REQ, buffer);
     if (package_send(request, client_socket_master) < 0) {
-        log_error(logger, "## No se pudo enviar el handshake al Master. IP=%s:%s", master_ip, master_port);
+        log_error(logger, "## No se pudo enviar el handshake al Master");
         goto clean;
     }
     package_destroy(request);
 
     t_package *response = package_receive(client_socket_master);
     if (response == NULL) {
-        log_error(logger, "## No se pudo recibir el handshake del Master. IP=%s:%s", master_ip, master_port);
+        log_error(logger, "## No se pudo recibir el handshake del Master");
         goto clean;
     }
 
     if (response->operation_code != OP_WORKER_HANDSHAKE_RES) {
-        log_error(logger, "## Handshake con Master fallido. IP=%s:%s", master_ip, master_port);
+        log_error(logger, "## Handshake con Master fallido");
         goto clean;
     }
 
-    log_info(logger, "## Handshake con Master exitoso. IP=%s:%s", master_ip, master_port);
+    log_info(logger, "## Handshake con Master exitoso");
+    log_debug(logger, "## Operacion recibida: %d", response->operation_code);
 
     package_destroy(response);
     return client_socket_master;
