33aebead162d84386cf9e55bd408808d3e4f455a
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2025-09-26 16:44:37 +0000

    refactor

--- worker/src/connections/storage.c
+++ worker/src/connections/storage.c
@@ -13,25 +13,29 @@ int handshake_with_storage(const char *storage_ip,
 
 int get_block_size(int storage_socket, int *block_size) {
     t_log *logger = get_logger();
-    t_package *package_req = package_create(STORAGE_OP_WORKER_GET_BLOCK_SIZE_REQ, NULL);
-    if (!package_req) {
+    t_package *request = package_create(STORAGE_OP_WORKER_GET_BLOCK_SIZE_REQ, NULL);
+    if (!request) {
         log_error(logger, "Error al crear el paquete para solicitar el tamaño del bloque");
         return -1;
     }
-    package_send(package_req, storage_socket);
-    package_destroy(package_req);
-    t_package *package_res = package_receive(storage_socket);
-    if (!package_res) {
+    if(package_send(request, storage_socket) != 0) {
+        log_error(logger, "Error al enviar la solicitud de tamaño del bloque al Storage");
+        package_destroy(request);
+        return -1;
+    }
+    package_destroy(request);
+    t_package *response = package_receive(storage_socket);
+    if (!response) {
         log_error(logger, "Error al recibir la respuesta del tamaño del bloque");
         return -1;
     }
-    if (package_res->header.op_code != STORAGE_OP_WORKER_GET_BLOCK_SIZE_RES) {
+    if (response->operation_code != STORAGE_OP_WORKER_GET_BLOCK_SIZE_RES) {
         log_error(logger, "Tipo de paquete inesperado para la respuesta del tamaño del bloque");
-        package_destroy(package_res);
+        package_destroy(response);
         return -1;
     }
     
-    memcpy(block_size, package_res->payload, sizeof(int));
+    memcpy(block_size, response->buffer, sizeof(int));
     package_destroy(package_res);
     return 0;
 }
