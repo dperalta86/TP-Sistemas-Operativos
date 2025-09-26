6aa39d23d9724eeda2f600af92554113ff4655a9
Carlos Nahuel Morocho <carlos.nahuel.morocho@gmail.com>
2025-09-26 16:32:09 +0000

    feat(worker): se agrega función para obtener tam. de bloque

--- worker/src/connections/storage.c
+++ worker/src/connections/storage.c
@@ -10,3 +10,28 @@ int handshake_with_storage(const char *storage_ip,
                                  STORAGE_OP_WORKER_SEND_ID_RES,
                                  worker_id);
 }
+
+int get_block_size(int storage_socket, int *block_size) {
+    t_log *logger = get_logger();
+    t_package *package_req = package_create(STORAGE_OP_WORKER_GET_BLOCK_SIZE_REQ, NULL);
+    if (!package_req) {
+        log_error(logger, "Error al crear el paquete para solicitar el tamaño del bloque");
+        return -1;
+    }
+    package_send(package_req, storage_socket);
+    package_destroy(package_req);
+    t_package *package_res = package_receive(storage_socket);
+    if (!package_res) {
+        log_error(logger, "Error al recibir la respuesta del tamaño del bloque");
+        return -1;
+    }
+    if (package_res->header.op_code != STORAGE_OP_WORKER_GET_BLOCK_SIZE_RES) {
+        log_error(logger, "Tipo de paquete inesperado para la respuesta del tamaño del bloque");
+        package_destroy(package_res);
+        return -1;
+    }
+    
+    memcpy(block_size, package_res->payload, sizeof(int));
+    package_destroy(package_res);
+    return 0;
+}
