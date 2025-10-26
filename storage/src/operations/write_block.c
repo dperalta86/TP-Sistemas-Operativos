91cbce340e868d72d69afd15fdbcd2be4cf78b28
David Achas <dachassilva@frba.utn.edu.ar>
2025-10-26 19:16:17 +0000

    FInaliza proceso de escritura en bloque.

--- storage/src/operations/write_block.c
+++ storage/src/operations/write_block.c
@@ -1,99 +1,224 @@
 #include "write_block.h"
 
-t_package *write_block(t_package *package, t_client_data *client_data) {
+t_package *write_block(t_package *package) {
     uint32_t query_id;
     char *name = NULL;
     char *tag = NULL;
     uint32_t block_number;
     char *block_content = NULL;
+    t_package *retval = NULL;
 
-    int validation_result = validate_deserialization(package, &query_id, &name, &tag, &block_number, &block_content);
-    if(validation_result < 0) 
-    {
-      return NULL;
+    if (validate_deserialization(package, &query_id, &name, &tag, &block_number, &block_content) < 0) {
+        goto end;
     }
 
-    if(!file_dir_exists(g_storage_config->mount_point, name, tag)) 
+    lock_file(name, tag);
+
+    if(!file_dir_exists(name, tag))     
     {
       log_error(g_storage_logger, "## Query ID: %d - El directorio %s:%s no existe.", query_id, name, tag);
-      free(name);
-      free(tag);
-      free(block_content);
-
-      return prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, FILE_TAG_ALREADY_EXISTS);
+      retval = prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, FILE_TAG_MISSING);
+      goto cleanup_unlock;
     }
 
-    if(verify_metadata_status(g_storage_config->mount_point, name, tag, COMMITTED))
+    t_file_metadata *metadata = read_file_metadata(g_storage_config->mount_point, name, tag);
+    if (metadata == NULL)
+    {
+      log_error(g_storage_logger, "## Query ID: %d - No se pudo leer el metadata de %s:%s.", query_id, name, tag);
+      retval = prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, FILE_TAG_MISSING);
+      goto cleanup_unlock;
+    }
+    
+    if (strcmp(metadata->state, COMMITTED) == 0)
     {
       log_error(g_storage_logger, "## Query ID: %d - El archivo %s:%s ya está en estado 'COMMITTED' y no puede ser escrito.", query_id, name, tag);
-      free(name);
-      free(tag);
-      free(block_content);
-
-      return prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, FILE_ALREADY_COMMITTED);
+      retval = prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, FILE_ALREADY_COMMITTED);
+      goto cleanup_metadata;
     }
 
-    if(!logical_block_exists(g_storage_config->mount_point, name, tag, block_number))
+    char logical_block_path[PATH_MAX];
+    if(!logical_block_exists(name, tag, block_number, logical_block_path))
     {
       log_error(g_storage_logger, "## Query ID: %d - El bloque lógico %d no existe en %s:%s.", query_id, block_number, name, tag);
-      free(name);
-      free(tag);
-      free(block_content);
-
-      return prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, OP_OUT_OF_BOUNDS);
+      retval = prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, OP_OUT_OF_BOUNDS);
+      goto cleanup_metadata;
     }
 
+    if (!ph_block_has_many_hl(logical_block_path))
+    {
+      if (remove(logical_block_path) != 0)
+      {
+        log_error(g_storage_logger, "## Query ID: %d - No se pudo eliminar el hardlink %s.", query_id, logical_block_path);
+        retval = NULL;
+        goto cleanup_metadata;
+      }
+      
+      pthread_mutex_lock(&g_storage_bitmap_mutex);
 
+      t_bitarray *bitmap = NULL;
+      char *bitmap_buffer = NULL;
+      if(bitmap_load(&bitmap, &bitmap_buffer) < 0)
+      {
+        log_error(g_storage_logger, "# Query ID: %d - Fallo al cargar el bitmap.", query_id);
+        retval = NULL;
+        goto cleanup_bitmap_mutex;
+      }
 
+      ssize_t bit_index = get_free_bit_index(bitmap);
+      if (bit_index < 0)
+      {
+        log_error(g_storage_logger, "## Query ID: %d - No hay bloques físicos libres disponibles en el bitmap.", query_id);
+        retval = prepare_error_response(query_id, STORAGE_OP_BLOCK_WRITE_RES, NOT_ENOUGH_SPACE);
+        goto cleanup_bitmap;
+      }
+      
+      bitarray_set_bit(bitmap, (off_t)bit_index);
 
+      if (bitmap_persist(bitmap, bitmap_buffer) < 0)
+      {
+        retval = NULL;
+        goto cleanup_bitmap;
+      }
 
+      free(bitmap_buffer);  
+      destroy_bitmap();
+      pthread_mutex_unlock(&g_storage_bitmap_mutex);
 
+      char physical_block_path[PATH_MAX];
+      if(create_new_hardlink(query_id, logical_block_path, bit_index, physical_block_path) < 0)
+      {
+        retval = NULL;
+        goto cleanup_metadata;
+      }
+      
+      metadata->blocks[block_number] = (uint32_t)physical_block;
+      if(save_file_metadata(metadata) < 0) {
+        log_error(g_storage_logger, "## No se pudo guardar el metadata de %s:%s después de actualizar bloques.", name, tag);
+        retval = NULL;
+        goto cleanup_metadata;
+      }
 
+      destroy_file_metadata(metadata);
+    }
 
+    if(write_in_ph_block(query_id, logical_block_path, block_content) < 0)
+    {
+      retval = NULL;
+      goto cleanup_unlock;
+    }
+    
+    unlock_file(name, tag);
 
+  retval = package_create_empty(STORAGE_OP_BLOCK_WRITE_RES);
+  if(!retval)
+  {
+    log_error(g_storage_logger, "## Query ID: %d - Fallo al crear paquete de respuesta.", query_id);
+    retval = NULL;
+  }
 
-  t_package *response = package_create_empty(STORAGE_OP_BLOCK_WRITE_RES);
-  return response;
+  goto cleanup_var;
+
+cleanup_bitmap:
+  free(bitmap_buffer);  
+  destroy_bitmap();
+cleanup_bitmap_mutex:
+  pthread_mutex_unlock(&g_storage_bitmap_mutex);
+cleanup_metadata:
+  destroy_file_metadata(metadata);
+cleanup_unlock:
+  unlock(name, tag);
+cleanup_var:
+  free(name);
+  free(tag);
+  free(block_content);
+end:
+  return retval;
 }
 
 int validate_deserialization(t_package *package, uint32_t *query_id, char **name, char **tag, uint32_t *block_number, char **block_content) {
+  int retval = 0;
+
   if (!package_read_uint32(package, query_id)) 
   {
     log_error(g_storage_logger, "## Error al deserializar query_id de WRITE_BLOCK");
-    return -1;
+    retval = -1;
+    goto end;
   }
 
   *name = package_read_string(package);
   if (*name == NULL) 
   {
     log_error(g_storage_logger, "## Query ID: %d - Error al deserializar el nombre del file de WRITE_BLOCK", *query_id);
-    return -1;
+    retval = -1;
+    goto end;
   }
   
   *tag = package_read_string(package);
   if (*tag == NULL) 
   {
     log_error(g_storage_logger, "## Query ID: %d - Error al deserializar el tag del file de WRITE_BLOCK", *query_id);
-    free(*name);
-    return -1;
+    retval = -1;
+    goto clean_name;
   }
 
   if(!package_read_uint32(package, block_number))
   {
     log_error(g_storage_logger, "## Query ID: %d - Error al deserializar el número bloque de WRITE_BLOCK", *query_id);
-    free(*name);
-    free(*tag);
-    return -1;
+    retval = -1;
+    goto clean_tag;
   }
 
   *block_content = package_read_string(package);
   if(*block_content == NULL)
   {
     log_error(g_storage_logger, "## Query ID: %d - Error al deserializar el contenido a escribir de WRITE_BLOCK", *query_id);
-    free(*name);
-    free(*tag);
-    return -1;
+    retval = -1;
+    goto clean_tag;
   }
 
-  return 0;
+  return retval;
+
+clean_tag:
+  if(*tag) free(*tag);
+clean_name:
+  if(*name) free(*name);
+end:
+  return retval;
+}
+
+int create_new_hardlink(uint32_t query_id, char *logical_block_path, ssize_t bit_index, char *physical_block_path) {
+    char new_physical_block_path[PATH_MAX];
+    snprintf(new_physical_block_path, sizeof(new_physical_block_path), "%s/physical_blocks/block%04zd.dat", g_storage_config->mount_point, bit_index);
+
+    if (!physical_block_exists((uint32_t)bit_index, physical_block_path))
+    {
+      log_error(g_storage_logger, "## Query ID: %d - El archivo %s no existe.", query_id, physical_block_path);
+      return -1;
+    }
+
+    if (link(new_physical_block_path, logical_block_path) != 0) 
+    {
+      log_error(g_storage_logger, "## Query ID: %d - No se pudo crear el hard link de %s a %s.", query_id, new_physical_block_path, logical_block_path);
+      return -1;
+    }
+
+    return 0;
+}
+
+int write_in_ph_block(uint32_t query_id, const char *logical_block_path, const char *block_content) {
+    FILE *block_file = fopen(logical_block_path, "r+b");
+    if (block_file == NULL) {
+        log_error(g_storage_logger, "## Query ID: %d - No se pudo abrir el bloque %s para escritura.", query_id, logical_block_path);
+        return -1;
+    }
+
+    size_t bytes_written = fwrite(block_content, sizeof(char), strlen(block_content), block_file);
+    if (bytes_written != strlen(block_content)) {
+        log_error(g_storage_logger, "## Query ID: %d - Error al escribir en el bloque físico %s.", query_id, logical_block_path);
+        fclose(block_file);
+        return -1;
+    }
+
+    fclose(block_file);
+    return 0;
 }
--- storage/src/operations/write_block.h
+++ storage/src/operations/write_block.h
@@ -1,15 +1,65 @@
 #ifndef STORAGE_OPERATIONS_WRITE_BLOCK_H_
 #define STORAGE_OPERATIONS_WRITE_BLOCK_H_
 
+#include <stdio.h>
+#include <commons/bitarray.h>
 #include "connection/serialization.h"
 #include "connection/protocol.h"
 #include "globals/globals.h"
 #include "server/server.h"
 #include "utils/filesystem_utils.h"
+#include "file_locks.h"
 
 #define IN_PROGRESS "WORK_IN_PROGRESS"
 #define COMMITTED "COMMITTED"
 
-t_package* write_block(t_package *package);
+/**
+ * Implementa la lógica principal para la operación WRITE BLOCK del Storage.
+ * Deserializa la Query, valida los permisos, gestiona el espacio libre (bitmap),
+ * y realiza la reasignación de bloque físico y la escritura final de los datos en disco.
+ * 
+ * @param package El paquete serializado recibido del Worker.
+ * @return t_package* Un paquete de respuesta indicando éxito (código 0) o un error de protocolo. 
+ * Retorna NULL en caso de errores internos irrecuperables.
+ */
+t_package *write_block(t_package *package);
 
+/**
+ * Escribe el contenido de un bloque en el disco, a través del hardlink lógico.
+ * Asume que el bloque físico ya está apuntado por el hardlink lógico.
+ * Abre el archivo en modo r+b y escribe el contenido.
+ * 
+ * @param query_id ID de la Query asociada a la operación (para logging).
+ * @param logical_block_path Ruta completa al hardlink lógico del bloque.
+ * @param block_content Contenido a escribir.
+ * @return int 0 si la escritura es exitosa, -1 en caso de error de E/S.
+ */
+int write_in_ph_block(uint32_t query_id, const char *logical_block_path, const char *block_content);
+
+/**
+ * Crea un nuevo hardlink (en la ruta lógica) que apunta a un bloque físico.
+ * 
+ * @param query_id ID de la Query para logging.
+ * @param logical_block_path Ruta donde se creará el nuevo hardlink lógico.
+ * @param bit_index Índice del bit (número de bloque físico) asignado.
+ * @param physical_block_path Puntero donde se almacenará la ruta al bloque físico.
+ * @return int 0 si el hardlink se creó correctamente, -1 en caso de fallo.
+ */
+int create_new_hardlink(uint32_t query_id, char *logical_block_path, ssize_t bit_index, char *physical_block_path);
+
+/**
+ * Deserializa los datos necesarios para la operación WRITE BLOCK.
+ * Extrae de forma segura el ID de Query, el File Name, el Tag, el número de bloque
+ * y el contenido a escribir. Libera los punteros asignados (name, tag, block_content) 
+ * si la deserialización falla a mitad de proceso.
+ * 
+ * @param package El paquete serializado recibido.
+ * @param query_id Puntero donde se almacenará el ID de la Query.
+ * @param name Puntero al puntero donde se almacenará el nombre del File (debe ser liberado).
+ * @param tag Puntero al puntero donde se almacenará el Tag (debe ser liberado).
+ * @param block_number Puntero donde se almacenará el número de bloque lógico.
+ * @param block_content Puntero al puntero donde se almacenará el contenido (debe ser liberado).
+ * @return int 0 si la deserialización es exitosa, -1 si falla.
+ */
+int validate_deserialization(t_package *package, uint32_t *query_id, char **name, char **tag, uint32_t *block_number, char **block_content);
 #endif
