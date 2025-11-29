4434086794fcd9563c34e0eadfaec499881f428b
David Achas <dachassilva@frba.utn.edu.ar>
2025-11-29 17:13:49 +0000

    Ccrrige lock de diccionario.

--- storage/src/file_locks.c
+++ storage/src/file_locks.c
@@ -7,8 +7,6 @@
 
 void lock_file(const char *name, const char *tag, bool for_write) {
   char *key = string_from_format("%s:%s", name, tag);
-
-  pthread_mutex_lock(&g_storage_open_files_dict_mutex);
   t_file_mutex *fm = dictionary_get(g_open_files_dict, key);
 
   if (fm == NULL) {
@@ -22,14 +20,16 @@ void lock_file(const char *name, const char *tag, bool for_write) {
     }
 
     fm->ref_count = 0;
+
+    pthread_mutex_lock(&g_storage_open_files_dict_mutex);
     dictionary_put(g_open_files_dict, key, fm);
+    pthread_mutex_unlock(&g_storage_open_files_dict_mutex);
   }
 
   for_write ? pthread_rwlock_wrlock(&fm->mutex)
             : pthread_rwlock_rdlock(&fm->mutex);
   fm->ref_count++;
 
-  pthread_mutex_unlock(&g_storage_open_files_dict_mutex);
   free(key);
 
   return;
